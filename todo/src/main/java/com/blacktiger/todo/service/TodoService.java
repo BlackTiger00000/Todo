adddb90f119d1a9e43f86ed9e76e3b6f25ee5ee0
sectionr0 <sectionr0@gmail.com>
2022-01-09 19:17:27 +0900

    add Repository

--- todo/src/main/java/com/blacktiger/todo/service/TodoService.java
+++ todo/src/main/java/com/blacktiger/todo/service/TodoService.java
@@ -1,11 +1,23 @@
 package com.blacktiger.todo.service;
 
+import com.blacktiger.todo.model.TodoEntity;
+import com.blacktiger.todo.persistence.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
+    @Autowired
+    private TodoRepository repository;
+
     public String testService(){
-        return "Test Service";
+        //TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        //TodoEntity 저장
+        repository.save(entity);
+        //TodoEntity 검색
+        TodoEntity saveEntity = repository.findById(entity.getId()).get();
+        return saveEntity.getTitle();
     }
 }
