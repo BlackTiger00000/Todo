d70804abefe6ff1f64b7a42509da0664ac966ddd
sectionr0 <sectionr0@gmail.com>
2022-01-09 18:27:40 +0900

    add Springboot

---
+++ todo/src/test/java/com/blacktiger/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacktiger.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
